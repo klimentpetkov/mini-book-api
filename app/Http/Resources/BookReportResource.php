cfca06affd214b6c19250e84f3263c6665ab4a06
Kliment Petkov <klimopetkov@gmail.com>
2025-11-25 17:14:53 +0200

    Updated: cs fixes

--- app/Http/Resources/BookReportResource.php
+++ app/Http/Resources/BookReportResource.php
@@ -3,8 +3,8 @@
 namespace App\Http\Resources;
 
 use App\Models\BookReport;
-use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
 
 /**
  * @mixin BookReport
